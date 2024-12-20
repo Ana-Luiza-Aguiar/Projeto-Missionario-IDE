9ed315340c4d112726d7d57122dd753744c67c08
Ana Luiza Aguiar <167457228+Ana-Luiza-Aguiar@users.noreply.github.com>
2024-12-20 14:59:58 -0300

    Arquivos principais do projeto

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+document.getElementById('formCadastro').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Cadastro enviado com sucesso!');
+});
+
+document.getElementById('formAcompanhamento').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('CPF verificado com sucesso!');
+});
